00fda890358837b9af283283da0b404694e3fdae
many-to-one <xllxlex@gmail.com>
2022-12-05 14:34:56 +0100

    1st com/a-z app load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {API_URL, apiOptions} from './components/Api';
+
 
 function App() {
+
+  const [search, setSearch] = useState('');
+
+  const [weather, setWeather] = useState({})
+
+  const searchPressed = () => {
+    console.log('search pressed')
+    fetch(`${API_URL}${search}`, apiOptions)
+    .then(response => response.json())
+	  .then(response => {
+      setWeather(response)
+    })
+	  .catch(err => console.error(err));
+  }; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='main-container'>
+
+        {/* HEADER */}
+
+      <div className='header'>
+        Weather - App
+      </div>
+
+      {/* SEARCH-BOX */}
+      
+      <div className='search-section'>
+        <input 
+          type='text' 
+          placeholder='Search...'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={searchPressed}>Search</button>
+      </div>
+
+      {/* LOCATION */}
+      <p>{weather.location.name}, {weather.location.country}</p>
+
+      {/* TEMPERATURE */}
+      <p>{weather.current.temp_c}°C</p>
+
+      {/* CONDITION-SUNNY */}
+      <p>{weather.current.cloud}</p>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Api.js
@@ -0,0 +1,9 @@
+export const apiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e888c42f67msh10e9b05c155346cp112e4djsncdfda2df318c',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+export const API_URL = 'https://weatherapi-com.p.rapidapi.com/current.json?q='
