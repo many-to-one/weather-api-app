7f13f7b941cea95bbd2c3464103eafa4cc6937a6
many-to-one <xllxlex@gmail.com>
2022-12-06 14:40:57 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import {API_URL, apiOptions} from './components/Api';
+import {AiOutlineSearch} from 'react-icons/ai'
 
 
 function App() {
@@ -11,60 +12,79 @@ function App() {
 
   const searchPressed = () => {
     console.log('search pressed')
-    fetch(`${API_URL}${search}`, apiOptions)
+
+    // fetch(`${API_URL}${search}`, apiOptions)
+	  // .then(response => response.json())
+	  // .then(response => console.log(response))
+	  // .catch(err => console.error(err));
+
+    fetch(`${API_URL}${search}&days=3`, apiOptions)
     .then(response => response.json())
 	  .then(response => {
       setWeather(response)
     })
-	  .catch(err => console.error(err));
+    .catch(err => console.error(err));
   }; 
 
   return (
+
     <div className="App">
 
-      <div className='main-container'>
+      <div className='top'>
 
-        {/* HEADER */}
+        <div className='title'>Weather</div>
 
-      <div className='header'>
-        Weather - App
+        <div className='search'>
+
+          <input 
+            className='input'
+            placeholder='Search...'
+            onChange={(e) => {setSearch(e.target.value)}}
+          />
+           <button className='btn' onClick={searchPressed}><AiOutlineSearch color='white' size={20}/></button>
+
+        </div>
+
       </div>
 
-      {/* SEARCH-BOX */}
-      
-      <div className='search-section'>
-        <input 
-          type='text' 
-          placeholder='Search...'
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button onClick={searchPressed}>Search</button>
-      </div>
+      {/* <p>{weather.location.name}</p> */}
 
-      <div className='location'>
-        {/* LOCATION */}
+       <div className='location'>
         <p>{weather.location.name}, {weather.location.country}</p>
       </div>
 
-      <div className='temp-discirp'>
+      <div className='temp'>
+        <p>{weather.current.temp_c}°C</p>
+      </div>
 
-        <div className='temp'>
-          {/* TEMPERATURE */}
-          <p>{weather.current.temp_c}°C</p>
+      <div className='bottom'>
+
+        <div className='discription'>
+          <span className='index'>Local time</span>
+          <span className='value'>{weather.location.localtime} </span>
+        <div/>  
+
+        <div className='discription'>  
+          <span className='index'>Feels like</span>
+          <span className='value'>{weather.current.feelslike_c}</span>
         </div>
 
-          {/* DESCRIPTION */}
-        <div className='description'>
-            {/* CLOUD */}
-            <p>Cloud:</p>
-            <p>{weather.current.cloud}</p>
+        <div className='discription'>   
+          <span className='index'>Wind</span>
+          <span className='value'>{weather.current.wind_mph}</span>
+        </div>
+
+        <div className='discription'> 
+          <span className='index'>Cloud</span>
+          <span className='value'>{weather.current.cloud}</span>
         </div>
 
       </div>
 
-      </div>
+      </div> 
 
     </div>
+
   );
 }
 
